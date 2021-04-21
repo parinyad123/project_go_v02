89fbf72bfa5a16a8c3252b5981eb69e0e5f792cf
parinyad123 <sci.equ@gmail.com>
2021-04-21 13:49:27 +0700

    add controllers dir into project

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	configs "project_go_v02/configs"
-	
 )
 
 func main() {
 	db := configs.Connect()
+	fmt.Println("db main = ", db)
 	// defer configs.Connect().Close()
 	defer db.Close()
 
--- models/model.go
+++ models/model.go
@@ -21,30 +21,30 @@ type SubSystem struct {
 	Telemetry []*Telemetry `pg:"rel:has-many"`	
 }
 
-// type Telemetry struct {
-// 	Id string `json:"id"`
-// 	Satellite_name string `json:"satellite_name"`
-// 	Subsystem_name string `json:"subsystem_name"`
-// 	TM_name string `json:"tm_name"`
-// 	Description string `json:"description"`
-// 	Image string `json:"image"`
-// 	TM_package string `json:"tm_package"`
-// }
-
 type Telemetry struct {
-	Id string 
-	Satellite_name string 
-	Subsystem_name string 
-	TM_name string 
-	Description string 
-	Image string 
-	TM_package string
+	Id string `json:"id"`
+	Satellite_name string `json:"satellite_name"`
+	Subsystem_name string `json:"subsystem_name"`
+	TM_name string `json:"tm_name"`
+	Description string `json:"description"`
+	Image string `json:"image"`
+	TM_package string `json:"tm_package"`
 }
+
+// type Telemetry struct {
+// 	Id string 
+// 	Satellite_name string 
+// 	Subsystem_name string 
+// 	TM_name string 
+// 	Description string 
+// 	Image string 
+// 	TM_package string
+// }
 
 func CreateSchema(db *pg.DB) error {
 	models := []interface{} {
-		// (*Satellite)(nil),
-		// (*SubSystem)(nil),
+		(*Satellite)(nil),
+		(*SubSystem)(nil),
 		(*Telemetry)(nil),
 	}
 
@@ -52,9 +52,9 @@ func CreateSchema(db *pg.DB) error {
 
 	for _, model := range models {
 		err := db.Model(model).CreateTable(&orm.CreateTableOptions{
-			Temp: true,
+			IfNotExists: true,
 		})
-		if err != nil {
+		if err != nil { 
 			fmt.Println("Error create table func")
 			return err
 			
